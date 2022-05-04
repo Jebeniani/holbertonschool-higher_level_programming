71a5353e356ef8f9ee0842e0943914463f7f0533
jebeniani <aymenjebeniani05@gmail.com>
2022-05-04 04:25:33 -0700

    unique addition

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     newl = my_list.copy()
     for idx, i in enumerate(my_list):
-        if i == 2:
-            newl[idx] = 89
+        if i == search:
+            newl[idx] = replace
     return newl
