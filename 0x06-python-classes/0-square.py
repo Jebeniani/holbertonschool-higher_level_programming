80bd0a9b9b4b4de882544b6dc0ae7ea783d60283
jebeniani <aymenjebeniani05@gmail.com>
2022-05-19 04:54:26 -0700

    OOP

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" implemening an empty class Square
-=======
-""" creating an empty class named Square
->>>>>>> abf3be12ea32f9b1b61e5a06da543acc335f98bd
+""" a class to create a square
 """
 
 
 class Square:
+    """ creating an empty class named Square
+    """
     def __init__(self):
         return
