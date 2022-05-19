a96338f33335f9fe871b7c470204cec512b77de6
jebeniani <aymenjebeniani05@gmail.com>
2022-05-19 05:22:12 -0700

    OOP

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class
+Defines a square class
+and has private instance size
+"""
+
+
+class Square:
+    """Square class
+    TypeError exception with the message size must be an integer
+    """
+    def __init__(self, size=0):
+        """__init__
+        initializing square
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
