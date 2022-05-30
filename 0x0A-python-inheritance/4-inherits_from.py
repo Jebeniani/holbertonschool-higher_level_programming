fe7d54fdaaedc1ac2d892e62298e3444bc970634
jebeniani <aymenjebeniani05@gmail.com>
2022-05-30 12:41:04 -0700

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ from the specified class
 def inherits_from(obj, a_class):
     """returns True if the object"""
 
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(obj.__class__, a_class) and type(obj) != a_class
