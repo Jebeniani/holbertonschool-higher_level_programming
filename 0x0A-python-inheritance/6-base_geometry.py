27f71428bc0391970b7cba0b6d76867d308baff6
jebeniani <aymenjebeniani05@gmail.com>
2022-05-30 12:51:03 -0700

    inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Public instance method that raises an Exception
-with the message area() is not implemented"""
+"""writing a class named BaseGeometry"""
 
 
 class BaseGeometry:
+    """creating a class"""
     def area(self):
-        """implementation"""
+        """raises an Exception with the
+        message area() is not implemented"""
         raise Exception("area() is not implemented")
