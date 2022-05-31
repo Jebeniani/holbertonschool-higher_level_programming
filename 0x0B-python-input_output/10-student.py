89eecc3f9bc91969e1ea75cf7f84ae2dc0f6cc5d
jebeniani <aymenjebeniani05@gmail.com>
2022-05-31 14:59:57 -0700

    input/output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""writing a class named Student"""
+
+
+class Student:
+    """creating the class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json method that retrieves 
+        a dictionary representation of a Student instance"""
+        dict = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    dict[i] = self.__dict__[i]
+            return(dict)
