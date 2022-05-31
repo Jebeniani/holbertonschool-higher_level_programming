e5a684bd5810bd1ac454e5fc24aefd6a6062494b
jebeniani <aymenjebeniani05@gmail.com>
2022-05-31 14:04:58 -0700

    input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a function that reads a text file and prints it to stdout"""
 
+
 def read_file(filename=""):
     """open with"""
     with open('my_file_0.txt', encoding='utf-8') as op:
