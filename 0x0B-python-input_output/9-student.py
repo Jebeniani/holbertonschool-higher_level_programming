701b82269ea734ae2c601459cd3ca4b2bde12d82
jebeniani <aymenjebeniani05@gmail.com>
2022-05-31 14:40:29 -0700

    input/output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""writing a class named Student"""
+
+
+class Student:
+    """creating the class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
