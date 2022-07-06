64cc3d2cc715a75956f4a02c58344b97b5309747
jebeniani <aymenjebeniani05@gmail.com>
2022-07-06 14:45:04 -0700

    No Homeland

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows 
 LEFT JOIN tv_shows_genres ON tv_shows_genres.show_id = tv_show_.id
-ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
