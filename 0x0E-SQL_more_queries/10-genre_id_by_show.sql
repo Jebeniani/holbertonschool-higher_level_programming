c9b9c109480ed5a91d46f8aa7a306e7943c43186
jebeniani <aymenjebeniani05@gmail.com>
2022-07-06 15:07:07 -0700

    the north remember

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows 
-RIGHT JOIN tv_shows_genres ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
