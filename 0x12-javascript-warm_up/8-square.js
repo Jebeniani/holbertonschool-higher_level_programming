d988d9485fa770273e82a2db82a14f9f1a236e59
jebeniani <aymenjebeniani05@gmail.com>
2022-08-01 14:41:33 -0700

    my first js square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+if (parseInt(process.argv[2]))
+{
+	let i;
+	for (i = 0; i < process.argv[2]; i++)
+	{
+		console.log('X'.repeat(parseInt(process.argv[2])));
+	}
+}
+else
+{
+	console.log('Missing size')
+}
