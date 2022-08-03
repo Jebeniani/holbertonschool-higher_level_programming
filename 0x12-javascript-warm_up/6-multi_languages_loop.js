a2496e6a11552a1775bfeea2bf081c6f4f4beea6
jebeniani <aymenjebeniani05@gmail.com>
2022-08-03 05:29:58 -0700

    task 1 looped

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const a = ["C is fun", "Python is cool", "JavaScript is amazing"];
-a.forEach(function(entry)
-{
+const a = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+a.forEach(function (entry) {
   console.log(entry);
 });
