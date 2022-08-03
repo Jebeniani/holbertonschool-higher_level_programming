b9756669680ee0e96afc152f9fc09da6006099a8
jebeniani <aymenjebeniani05@gmail.com>
2022-08-03 05:33:06 -0700

    we love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
 const liar = 'C is fun';
-if (process.argv[2] == null)
-{
-	console.log('Missing number of occurrences');
-}
-else
-{
-	let i;
-	for (i = 0; i < process.argv[2]; i++)
-	{
-		console.log(liar);
-	}
+if (process.argv[2] == null) {
+  console.log('Missing number of occurrences');
+} else {
+  let i;
+  for (i = 0; i < process.argv[2]; i++) {
+    console.log(liar);
+  }
 }
