5f4d4bd2c6d06eb8f0cbe2820791b45925828b28
jebeniani <aymenjebeniani05@gmail.com>
2022-08-03 05:42:05 -0700

    factorial in js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
-function factorial (n)
-{
-	const num = parseInt(n);
-	if (isNaN(num))
-	{
-		return (1);
-	}
-	if (n === 0)
-	{
-		return (1);
-	}
-	if (n < 0)
-	{
-		return (-1);
-	}
-		return (n * factorial(n - 1));
+function factorial (n) {
+  const num = parseInt(n);
+  if (isNaN(num)) {
+    return (1);
+  }
+  if (n === 0) {
+    return (1);
+  }
+  if (n < 0) {
+    return (-1);
+  }
+  return (n * factorial(n - 1));
 }
 console.log(factorial(process.argv[2]));
