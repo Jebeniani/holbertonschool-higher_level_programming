6cfc54ec91560f54ea9225156cde787e4dee96ed
Jebeniani <aymenjebeniani05@gmail.com>
2022-08-19 19:00:38 +0100

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,11 @@ import urllib.error
 import urllib.request                                               
 
 
+if __name__ == "__main__": 
+    req = urllib.request.Request(argv[1])
 
-if __name__ == "__main__":                                          
-    req = urllib.request.Request(argv[1])                       
-
-        try:                                                        
-            with urllib.request.urlopen(req) as response:       
-                print(response.read().decode('utf-8'))      
-        except urllib.error.HTTPError as err:                       
-            print("Error code: {}".format(err.code)) 
+        try:
+            with urllib.request.urlopen(req) as response:
+                print(response.read().decode('utf-8'))
+        except urllib.error.HTTPError as err:
+            print("Error code: {}".format(err.code))
