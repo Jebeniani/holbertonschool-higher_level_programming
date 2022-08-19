f4041638e72961785961187deaa8f0ae4fe06fc2
Jebeniani <aymenjebeniani05@gmail.com>
2022-08-19 23:57:46 +0100

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    req = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    .json().get('id')
+    req = requests.get('https://api.github.com/user', auth=(argv[1],
+        argv[2])).json().get('id')
 
     print(req)
